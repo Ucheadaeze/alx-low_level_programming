66e3d927e1685f54e9180ccf31547477eda3e63f
Ucheadaeze <ucheigbokwe25@gmail.com>
2022-09-08 17:18:16 -0700

    Task hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
 * main - print the string in the put function
 *
-* Description: using the main funtion
+* Description: using the main function
 * this program prints "Programming is like building a multilingual puzzle
 * Return: 0
 */
 int main(void)
 {
-         puts("\"Programming is like building a multilingual puzzle");
-         return (0);
-}	 
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}

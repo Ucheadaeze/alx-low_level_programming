505c6ea9d4973e40339beb97e4076c832cc3af83
Ucheadaeze <ucheigbokwe25@gmail.com>
2022-09-11 03:33:35 -0700

    Task 8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - print if the number is positive, zero, or negative 
+ * main - print if the number is positive, zero, or negative
  *
  * Description: using the main function
- * this program prints "Programming is positive, zero, or negative 
+ * this program prints "Programming is positive, zero, or negative
  * Return: 0
  */
 int main(void)
@@ -14,7 +14,7 @@ for (c = '0'; c <= '9'; c++)
 	putchar(c);
 }
 for (c = 'a'; c <= 'f'; c++)
-{ 
+{
 	putchar(c);
 }
 putchar ('\n');

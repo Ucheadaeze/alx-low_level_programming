a45cc80d6401179143d8b059e812b1f698562582
Ucheadaeze <ucheigbokwe25@gmail.com>
2022-09-13 14:06:17 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - functions to print abc
+ *
+ * Return:0
+ */
+void print_alphabet(void)
+{
+	chat c;
+
+	for (c= 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+_putchar('\n');
+}	

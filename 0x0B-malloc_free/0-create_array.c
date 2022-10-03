5a45d38c79c9ae696d6d513f4116b7f11a922a94
Ucheadaeze <ucheigbokwe25@gmail.com>
2022-10-03 15:07:40 -0700

    Task 0 through 4

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#incude "main.h"
+
+/**
+ * create_array - creates an array of charts, and
+ * initializes it with a specific char.
+ * @size: size of the array
+ * @c: Character to insert
+ * Return: NULL if size is zero or if it falls,
+ * pointer to array if everything is normal.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	unsigned int index;
+
+	if (size == 0)
+		return (NULL);
+	array = malloc(sizeof(char) * size);
+
+	if (array == NULL)
+		return (NULL);
+	for (index = 0; index < size; index++)
+		aray[index] = c;
+
+	return (array);
+}

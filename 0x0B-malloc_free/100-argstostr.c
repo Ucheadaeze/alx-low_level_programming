62502f17c810bdec61837636431d64dfc57c9ede
Ucheadaeze <ucheigbokwe25@gmail.com>
2022-10-04 15:20:46 -0700

    Task 4

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,57 +1,43 @@
 #include "main.h"
-
 /**
- * len - returns length of str
- * @str: string counted
- * Return: returns the length
- */
-int len(char *str)
-{
-		int len = 0
-
-		if (str != NULL)
-		{
-			while (str[len])
-				len++;
-		}
-	return (len);
-}
-
-/**
- * argstostr - a function that concatenates all the arguments of your program
- * @ac: count of args passed to the function
- * @av: array of arguments
- *
- * Return: pointer to the new string
+ * argstostr -prints args
+ * @ac: takes in width of grid
+ * @av: height of grid
+ * Return: the args one line at a time
  */
 
 char *argstostr(int ac, char **av)
 {
-	char *new_string = NULL;
-	int k = 0, i = ac, j, sum = 0, temp = 0;
+	char *str;
+	int count = 0, a = 0, b = 0, c = 0;
 
 	if (ac == 0 || av == NULL)
 		return (NULL);
-
-	while (ac--)
-		sum += (len(av[ac]) + 1);
-	new_string = (char *) malloc(sum + 1);
-
-	if (new_string != NULL)
+	while (a < ac)
 	{
-		while (k < i)
+		b = 0;
+		while (av[a][b] != '\0')
 		{
-			for (j = 0; av[k][j] != '\0'; j++)
-				new_string[j + temp] = av[k][j];
-			new_string[temp + j] = '\n';
-			temp += (j + 1);
-			k++;
+			count++;
+			b++;
 		}
-		new_string[temp] = '\0';
+		a++;
 	}
-	else
+	count = count + ac + 1;
+	str = malloc(sizeof(char) * count);
+	if (str == NULL)
 	{
 		return (NULL);
 	}
-	return (new_string);
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
+	}
+	return (str);
 }
